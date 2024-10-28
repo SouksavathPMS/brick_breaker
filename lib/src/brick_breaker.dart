89cc96d52fdfa34115cede52b34fe81a2f14b02b
Souksavath Phommaseng <kyedev@Souksavaths-MacBook-Pro.local>
2024-10-28 11:55:22 +0700

    Adding ball and bat

--- lib/src/brick_breaker.dart
+++ lib/src/brick_breaker.dart
@@ -1,12 +1,18 @@
 import 'dart:async';
+import 'dart:math' as math;
+import 'dart:ui';
 
 import 'package:brick_breaker/src/config.dart';
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-import 'components/play_area.dart';
+import 'components/components.dart';
 
-class BrickBreaker extends FlameGame {
+class BrickBreaker extends FlameGame
+    with HasCollisionDetection, KeyboardEvents {
   BrickBreaker()
       : super(
           camera: CameraComponent.withFixedResolution(
@@ -15,6 +21,7 @@ class BrickBreaker extends FlameGame {
           ),
         );
 
+  final rand = math.Random();
   double get width => size.x;
   double get height => size.y;
 
@@ -23,5 +30,35 @@ class BrickBreaker extends FlameGame {
     super.onLoad();
     camera.viewfinder.anchor = Anchor.topLeft;
     world.add(PlayArea());
+    world.add(
+      Ball(
+        velocity: Vector2((rand.nextDouble() - 0.05 * width), height * 0.02)
+          ..normalize()
+          ..scale(height / 4),
+        position: size / 2,
+        radius: ballRadius,
+      ),
+    );
+    world.add(
+      Bat(
+        size: Vector2(batWidth, batHeight),
+        cornerRadius: const Radius.circular(ballRadius / 2),
+        position: Vector2(width / 2, height * 0.95),
+      ),
+    );
+    debugMode = true;
+  }
+
+  @override
+  KeyEventResult onKeyEvent(
+      KeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    super.onKeyEvent(event, keysPressed);
+    switch (event.logicalKey) {
+      case LogicalKeyboardKey.arrowLeft:
+        world.children.query<Bat>().first.moveBy(-batStep);
+      case LogicalKeyboardKey.arrowRight:
+        world.children.query<Bat>().first.moveBy(batStep);
+    }
+    return KeyEventResult.handled;
   }
 }

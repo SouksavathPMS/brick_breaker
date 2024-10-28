46e85f99e3375da734ce52e17abf137122924eaa
Souksavath Phommaseng <kyedev@Souksavaths-MacBook-Pro.local>
2024-10-28 10:48:34 +0700

    Added brick breaker game

---
+++ lib/src/brick_breaker.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+import 'package:brick_breaker/src/config.dart';
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+
+import 'components/play_area.dart';
+
+class BrickBreaker extends FlameGame {
+  BrickBreaker()
+      : super(
+          camera: CameraComponent.withFixedResolution(
+            width: gameWidth,
+            height: gameHeight,
+          ),
+        );
+
+  double get width => size.x;
+  double get height => size.y;
+
+  @override
+  FutureOr<void> onLoad() {
+    super.onLoad();
+    camera.viewfinder.anchor = Anchor.topLeft;
+    world.add(PlayArea());
+  }
+}
